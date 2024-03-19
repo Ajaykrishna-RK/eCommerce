d182a878c17dc5f1a04bcae77ae3f82534c21d00
Ajay <ajayajuu73@gmail.com>
2024-03-19 21:24:52 +0530

    api : issue solved no 2

--- server/controllers/seller/productCrud.js
+++ server/controllers/seller/productCrud.js
@@ -3,8 +3,19 @@ const addProduct = async (req, res) => {
     const { productName, description, price, category } = req.body;
     const imagePath = req.file.path;
 
-    res.json({ image: imagePath });
-  } catch (err) {}
+    // Process the product details and send back a response
+    const product = {
+      productName,
+      description,
+      price,
+      category,
+      image: imagePath,
+    };
+
+    res.status(200).json({ message: "Product added successfully", product });
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 module.exports = { addProduct };
